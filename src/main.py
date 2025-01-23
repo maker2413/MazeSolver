871cf8b38b95da33c67fe828cc40bba60f2a0bcd
maker2413 <maker2413@gmail.com>
2025-01-22 22:46:32 -0600

    started maze solver program

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from tkinter import Tk, BOTH, Canvas
+
+def main():
+    print("hello world")
+
+# This line ensures the main() function is only called when this file is run
+# directly; it won't run if it's imported as a module.
+if __name__ == "__main__":
+    main()
