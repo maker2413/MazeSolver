ca5d8588f44a97459c27c7cf0fbd2c68af544126
maker2413 <maker2413@gmail.com>
2025-01-24 11:41:22 -0600

    able to draw lines using tkinter

--- src/main.py
+++ src/main.py
@@ -1,7 +1,17 @@
-from tkinter import Tk, BOTH, Canvas
+from line import Line
+from point import Point
+from window import Window
 
 def main():
-    print("hello world")
+    win = Window(800, 600)
+
+    point1 = Point(10, 45)
+    point2 = Point(100, 45)
+    line1 = Line(point1, point2)
+
+    win.draw_line(line=line1, fill_color="red")
+
+    win.wait_for_close()
 
 # This line ensures the main() function is only called when this file is run
 # directly; it won't run if it's imported as a module.
--- src/window.py
+++ src/window.py
@@ -1,4 +1,27 @@
+from tkinter import Tk, BOTH, Canvas
+
 class Window():
     def __init__(self, width, height):
-        self.width = width
-        self.height = height
+        self.__root = Tk()
+        self.__root.title("Maze Solver")
+        self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
+        self.__canvas.pack(fill=BOTH, expand=1)
+        self.__is_running = False
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+
+    def wait_for_close(self):
+        self.__is_running = True
+        while self.__is_running:
+            self.redraw()
+
+        print("Maze Solver has been closed...")
+
+    def draw_line(self, line, fill_color):
+        line.draw(self.__canvas, fill_color)
+
+    def close(self):
+        self.__is_running = False
